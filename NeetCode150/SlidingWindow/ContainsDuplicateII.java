da291198f28810e1adf93efc12410aafde03dccc
Nurzhas Nurbayev <85830318+nurzhasj@users.noreply.github.com>
2023-06-11 23:04:58 +0600

    Update ContainsDuplicateII.java

--- NeetCode150/SlidingWindow/ContainsDuplicateII.java
+++ NeetCode150/SlidingWindow/ContainsDuplicateII.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0; i<nums.length; i++)
-        {
-            if(map.containsKey(nums[i]))
-            {
-                int pre = map.get(nums[i]);
-                if(i-pre<=k)
-                return true;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int index = map.get(nums[i]);
+
+                if (Math.abs(i - index) <= k) {
+                    return true;
+                }
             }
+
             map.put(nums[i], i);
         }
-     
+
         return false;
     }
 }
